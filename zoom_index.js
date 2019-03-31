85378dc748902fe7f40d405b973d0a155724519f
Simion Cristian Robert <cristian.simion@s.utm.ro>
2019-03-31 23:55:02 +0300

    Update Description and add search

---
+++ zoom_index.js
@@ -0,0 +1,252 @@
+dictwords = ["adventure 0 42 112 1 18 20 2 18 20 3 18 20 4 18 20 5 18 20 6 18 32 7 42 112 8 18 96 9 18 64 10 18 20",
+	"books 0 20 64 1 10 16 2 20 72 3 20 80 4 20 72 5 20 72 6 20 96 7 20 64 8 20 64 9 20 64 10 20 80",
+	"discounts 0 20 96 1 10 8 2 20 36 3 20 36 4 20 36 5 20 36 6 10 32 7 10 32 8 10 32 9 10 64 10 10 8",
+	"home 0 10 64 1 10 32 2 10 32 3 10 32 4 10 32 5 10 32 6 10 64 7 10 64 8 10 64 9 10 64 10 10 32",
+	"products 0 10 64 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16 6 10 64 7 10 64 8 10 64 9 10 64 10 10 32",
+	"fiction 0 18 96 1 18 20 2 18 20 3 27 84 4 18 20 5 18 20 6 18 96 7 18 96 8 35 96 9 28 96 10 18 20",
+	"children&#39;s 0 10 64 1 10 16 2 10 16 3 10 16 4 10 16 5 10 16 6 20 96 7 10 64 8 10 64 9 10 64 10 10 16",
+	"science 0 18 32 1 18 10 2 18 10 3 18 10 4 27 74 5 18 10 6 18 32 7 18 32 8 18 40 9 27 64 10 18 18",
+	"tehnology 0 18 32 1 18 10 2 18 10 3 18 10 4 18 10 5 18 10 6 18 32 7 18 32 8 18 96 9 18 64 10 18 10",
+	"discount 0 40 32 1 40 6 2 40 6 3 40 6 4 40 6 5 40 6 6 40 32 7 40 32 8 40 120 9 40 96 10 40 6",
+	"children 0 10 32 1 10 2 2 20 66 3 10 2 4 10 2 5 10 2 6 20 32 7 10 32 8 10 16 9 10 64 10 10 4",
+	"nightinglae 0 10 32",
+	"kristin 0 10 32 7 10 32",
+	"hannah 0 10 32 7 10 16",
+	"10 0 26 36",
+	"alice&#39;s 0 10 16 7 10 16",
+	"wonderland 0 10 16 7 10 16",
+	"lewis 0 10 16 7 10 16",
+	"carroll 0 10 16 7 10 16",
+	"20 0 18 24",
+	"quiet 0 10 16 7 10 16",
+	"western 0 10 16 7 10 16",
+	"front 0 10 16 7 10 16",
+	"maria 0 10 16 7 10 8",
+	"remarque 0 10 16 7 10 8",
+	"15 0 18 10",
+	"anne 0 10 8 7 10 8",
+	"green 0 10 8 7 10 8",
+	"gables 0 10 8 7 10 8",
+	"lucy 0 10 8 7 10 8",
+	"maud 0 10 8 7 10 8",
+	"montgomery 0 10 8 7 10 8",
+	"18 0 18 9",
+	"gulliver&#39;s 0 10 8 7 10 8",
+	"travels 0 10 8 7 10 8",
+	"jonathan 0 10 8 7 10 8",
+	"swift 0 10 8 7 10 8",
+	"kingdon 0 10 4 7 10 4",
+	"copper 0 10 4 7 10 4",
+	"s.a 0 10 4 7 10 4",
+	"chakraborty 0 10 4 7 10 4",
+	"secret 0 10 4 7 10 4",
+	"garden 0 10 4 7 10 4",
+	"francess 0 10 4 7 10 4",
+	"burnett 0 10 4 7 10 4",
+	"twelfth 0 10 2 7 10 2",
+	"night 0 10 2 7 10 2",
+	"william 0 10 2 7 10 2",
+	"shakespeare 0 10 2 7 10 2",
+	"war 0 10 2 7 10 2",
+	"wolf 0 10 2 7 10 2",
+	"bernard 0 10 2 7 10 2",
+	"cornwell 0 10 2 7 10 2",
+	"16 0 10 2",
+	"world 0 10 1 7 10 1",
+	"without 0 10 1 7 10 1",
+	"end 0 10 1 7 10 1",
+	"ken 0 10 1 7 10 1",
+	"follett 0 10 1 7 10 1",
+	"book 0 9 128 1 9 128 2 9 128 3 9 128 4 9 128 5 9 128 6 9 128 7 9 128 8 9 128 9 9 128 10 9 128",
+	"store 0 9 128 1 9 128 2 9 128 3 9 128 4 9 128 5 9 128 6 9 128 7 9 128 8 9 128 9 9 128 10 9 128",
+	"adventure.html 0 6 64 7 6 64",
+	"index.html 1 6 64",
+	"children.html 2 6 64 6 6 64",
+	"fiction.html 3 6 64 8 6 64",
+	"science.html 4 6 64 9 6 64",
+	"technology 5 12 64 10 12 64",
+	"technology.html 5 6 64 10 6 64",
+	"five 6 18 18",
+	"feet 6 10 16",
+	"apart 6 10 16",
+	"rachael 6 10 16",
+	"lippincott 6 10 16",
+	"gravity 6 10 16",
+	"falls 6 10 16",
+	"journal 6 10 16",
+	"alex 6 10 16",
+	"hirsch 6 10 16",
+	"harry 6 10 8",
+	"potter 6 10 8",
+	"box 6 10 8 8 10 32",
+	"set 6 10 8 8 10 16",
+	"complete 6 10 8",
+	"collection 6 10 8",
+	"j.k 6 26 11",
+	"rowling 6 26 11",
+	"man&#39;s 6 10 8 9 10 16",
+	"search 6 10 8 9 10 16",
+	"meaning 6 10 8 9 10 16",
+	"viktor 6 10 4 9 10 16",
+	"frankl 6 10 4 9 10 16",
+	"queen 6 10 4",
+	"air 6 10 4",
+	"darkness 6 10 4",
+	"cassandra 6 10 4",
+	"clare 6 10 4",
+	"crimes 6 10 2",
+	"grindelwald 6 10 2",
+	"fourth 6 10 2",
+	"closest 6 10 2",
+	"nights 6 10 2",
+	"freddy&#39;s 6 10 2",
+	"scott 6 10 2",
+	"cawthon 6 10 2",
+	"tales 6 10 1",
+	"beedle 6 10 1",
+	"bard 6 10 1",
+	"nightingale 7 10 32",
+	"bird 8 10 32",
+	"josh 8 10 32",
+	"malerman 8 10 32",
+	"fire 8 10 32",
+	"blood 8 10 32",
+	"george 8 10 32",
+	"r.r 8 10 32",
+	"martin 8 10 32",
+	"hidden 8 10 32",
+	"bodies 8 10 32",
+	"caroline 8 18 40",
+	"kepnes 8 18 40",
+	"film 8 10 32",
+	"tie 8 10 32",
+	"stephen 8 10 32",
+	"king 8 18 48",
+	"scars 8 10 16",
+	"leigh 8 10 16",
+	"bardugo 8 10 16",
+	"metro 8 10 16",
+	"2035 8 10 16",
+	"dmitry 8 10 16",
+	"glukhovsky 8 10 16",
+	"tattooist 8 10 16",
+	"auschwitz 8 10 16",
+	"heather 8 10 16 9 10 16",
+	"morris 8 10 16 9 10 16",
+	"hobbit 8 10 16",
+	"lord 8 10 16",
+	"rings 8 10 16",
+	"deluxe 8 10 16",
+	"pocket 8 10 16",
+	"boxed 8 10 16",
+	"j.r.r 8 10 16",
+	"tolkien 8 10 16",
+	"12 9 10 32",
+	"rules 9 10 32",
+	"life 9 10 32",
+	"jordan 9 10 32",
+	"peterson 9 10 32",
+	"behave 9 10 32",
+	"robert 9 10 32",
+	"sapolsky 9 10 32",
+	"educated 9 10 32",
+	"tara 9 10 32",
+	"westover 9 10 32",
+	"factfulness 9 10 16",
+	"hans 9 10 16",
+	"rosling 9 10 16",
+	"homoe 9 10 16",
+	"deus 9 10 16",
+	"yuval 9 18 24",
+	"noah 9 18 24",
+	"harari 9 18 24",
+	"milk 9 10 16",
+	"honey 9 10 16",
+	"sapiens 9 10 8",
+	"thinking 9 10 8",
+	"fast 9 10 8",
+	"slow 9 10 8",
+	"daniel 9 10 8",
+	"kahneman 9 10 8",
+	"this 10 10 2",
+	"category 10 10 2",
+	"currently 10 10 2",
+	"under 10 10 1",
+	"development 10 10 1"];
+skipwords = ["and",
+	"or",
+	"the",
+	"it",
+	"is",
+	"an",
+	"on",
+	"we",
+	"us",
+	"to",
+	"of",
+	"has",
+	"be",
+	"all",
+	"for",
+	"in",
+	"as",
+	"so",
+	"are",
+	"that",
+	"can",
+	"you",
+	"at",
+	"its",
+	"by",
+	"have",
+	"with",
+	"into"];
+var STR_FORM_SEARCHFOR = "Search for:";
+var STR_FORM_SUBMIT_BUTTON = "Submit";
+var STR_FORM_RESULTS_PER_PAGE = "Results per page:";
+var STR_FORM_CATEGORY = "Category:";
+var STR_FORM_CATEGORY_ALL = "All";
+var STR_FORM_MATCH = "Match:";
+var STR_FORM_ANY_SEARCH_WORDS = "any search words";
+var STR_FORM_ALL_SEARCH_WORDS = "all search words";
+var STR_FORM_DATE_FROM = "From:";
+var STR_FORM_DATE_TO = "To:";
+var STR_FORM_DATE_BUTTON = "&laquo;";
+var STR_NO_QUERY = "No search query entered.";
+var STR_RESULTS_FOR = "Search results for:";
+var STR_RESULTS_IN_ALL_CATEGORIES = "in all categories";
+var STR_RESULTS_IN_CATEGORY = "in category";
+var STR_POWEREDBY = "Search powered by";
+var STR_NO_RESULTS = "No results";
+var STR_RESULT = "result";
+var STR_RESULTS = "results";
+var STR_PHRASE_CONTAINS_COMMON_WORDS = "Your search query contained too many common words to return the entire set of results available. Please try again with a more specific query for better results.";
+var STR_SKIPPED_FOLLOWING_WORDS = "The following word(s) are in the skip word list and have been omitted from your search:";
+var STR_SKIPPED_PHRASE = "Note that you can not search for exact phrases beginning with a skipped word";
+var STR_SUMMARY_NO_RESULTS_FOUND = "No results found.";
+var STR_SUMMARY_FOUND_CONTAINING_ALL_TERMS = "found containing all search terms.";
+var STR_SUMMARY_FOUND_CONTAINING_SOME_TERMS = "found containing some search terms.";
+var STR_SUMMARY_FOUND = "found.";
+var STR_PAGES_OF_RESULTS = "pages of results.";
+var STR_MORETHAN = "More than";
+var STR_POSSIBLY_GET_MORE_RESULTS = "You can possibly get more results searching for";
+var STR_ANY_OF_TERMS = "any of the terms";
+var STR_ALL_CATS = "all categories";
+var STR_CAT_SUMMARY = "Refine your search by category:";
+var STR_DIDYOUMEAN = "Did you mean:";
+var STR_OR = "or";
+var STR_RECOMMENDED = "Recommended links";
+var STR_SORTEDBY_RELEVANCE = "Sorted by relevance";
+var STR_SORTBY_RELEVANCE = "Sort by relevance";
+var STR_SORTBY_DATE = "Sort by date";
+var STR_SORTEDBY_DATE = "Sorted by date &#9660;";
+var STR_SORTEDBY_DATE_ASC = "Sorted by date &#9650;";
+var STR_RESULT_TERMS_MATCHED = "Terms matched: ";
+var STR_RESULT_SCORE = "Score: ";
+var STR_RESULT_URL = "URL:";
+var STR_RESULT_PAGES = "Result Pages:";
+var STR_RESULT_PAGES_PREVIOUS = "Previous";
+var STR_RESULT_PAGES_NEXT = "Next";
+var STR_SEARCH_TOOK = "Search took";
+var STR_SECONDS = "seconds";
+var STR_MAX_RESULTS = "You have requested more results than served per query. Please try again with a more precise query.";
